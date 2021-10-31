f4093bea38b3d223a4124728c21498ba43eb94d2
hakanolgun <kaptanhakanolgun@gmail.com>
2021-10-31 13:40:18 +0300

    select component ready

--- src/components/Select.js
+++ src/components/Select.js
@@ -2,11 +2,19 @@ import { useContext } from "react";
 import WeatherContext from "../context/WeatherContext";
 
 export default function Select() {
-  const { cities } = useContext(WeatherContext);
+  const { cities, city, setCity } = useContext(WeatherContext);
+
+  function handleChangeCity(e) {
+    for (let i = 0; i < cities.length; i++) {
+      if (e.target.value === cities[i].name) {
+        setCity(cities[i]);
+      }
+    }
+  }
 
   return (
     <div>
-      <select>
+      <select value={city.name} onChange={handleChangeCity}>
         {cities.map((city, i) => {
           return (
             <option key={i} value={city.name}>
--- src/context/WeatherContext.js
+++ src/context/WeatherContext.js
@@ -1,11 +1,15 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import cities from "./data";
 
 const WeatherContext = createContext();
 
 export const WeatherProvider = ({ children }) => {
+  const [city, setCity] = useState(cities[54]);
+
   const values = {
     cities,
+    city,
+    setCity
   };
 
   return (
